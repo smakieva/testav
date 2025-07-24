4f9c1abd93742699eceb3738d64ebc713d93a361
Moonbot-Tech <ghab@moonbot.tech>
2025-07-24 23:40:56 +0300

    Add files via upload

--- Unit1.pas
+++ Unit1.pas
@@ -3,7 +3,7 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  System.Threading,
+  System.Threading, System.SyncObjs,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Controls.Presentation, FMX.StdCtrls;
 
 type
@@ -22,8 +22,10 @@ type
   end;
 
 var
-  Form1:        TForm1;
-  p:            array [0..1000] of pointer;
+  Form1:          TForm1;
+  p:              array [0..1000] of pointer;
+  TaskFill:       TThread;
+  TaskClear:      TThread;
 
 
 implementation
@@ -32,49 +34,55 @@ implementation
 
 procedure TForm1.bOpenFileClick(Sender: TObject);
 begin
-
   for var k := 0 to 1000 do
     p[k] := nil;
 
-  TThread.CreateAnonymousThread(
-  procedure
-  var
-    k:    integer;
-    N:    integer;
-  begin
-    repeat
+  If TaskFill = nil then begin
+    TaskFill := TThread.CreateAnonymousThread(
+    procedure
+    var
+      k:    integer;
+      N:    integer;
+    begin
+      repeat
 
-      for k := 0 to 1000 do If p[k] = nil then begin
-        N := random(10000);
-        GetMem(p[k], N);
-        FillChar(p[k]^, N, random(255));
-      end;
+        for k := 0 to 1000 do If p[k] = nil then begin
+          N := random(10000);
+          GetMem(p[k], N);
+          FillChar(p[k]^, N, random(255));
+        end;
 
-      Sleep(10);
+        Sleep(10);
 
-    until false;
+      until false;
 
-  end).start;
+    end);
 
-  TThread.CreateAnonymousThread(
-  procedure
-  var
-    k:    integer;
-    N:    integer;
-  begin
-    repeat
+    TaskFill.Start;
+  end;
 
-      Sleep(10);
+  If TaskClear = nil then begin
+    TaskClear := TThread.CreateAnonymousThread(
+    procedure
+    var
+      k:    integer;
+      N:    integer;
+    begin
+      repeat
 
-      for k := 0 to 1000 do If p[k] <> nil then begin
-        FreeMem(p[k]);
-        p[k] := nil;
-      end;
+        Sleep(10);
 
-    until false;
+        for k := 0 to 1000 do If p[k] <> nil then begin
+          FreeMem(p[k]);
+          p[k] := nil;
+        end;
 
-  end).start;
+      until false;
 
+    end);
+
+    TaskClear.Start;
+  end;
 
   OpenDialog1.Execute;
 end;
