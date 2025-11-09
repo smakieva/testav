84b846bf6e510664779cd665f6196d9984efe080
smakieva <sophiemakieva3876@gmail.com>
2025-11-09 22:42:39 +0300

    Bugfixes Unit1.pas

--- Unit1.pas
+++ Unit1.pas
@@ -1,14 +1,15 @@
 unit Unit1;
+
 interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   System.Threading, System.SyncObjs,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Controls.Presentation, FMX.StdCtrls;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls;
 
 type
   TForm1 = class(TForm)
-
     Popup1: TPopup;
     bOpenFile: TButton;
     bShowPopup: TButton;
@@ -16,81 +17,139 @@ type
     procedure bOpenFileClick(Sender: TObject);
     procedure bShowPopupClick(Sender: TObject);
   private
-    { Private declarations }
+    FLck: TObject;
+    FStop: Boolean;
+    FSize: array[0..1000] of Integer;
   public
-    { Public declarations }
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
   end;
 
 var
-  Form1:          TForm1;
-  p:              array [0..1000] of pointer;
-  TaskFill:       TThread;
-  TaskClear:      TThread;
-
+  Form1: TForm1;
+  p: array[0..1000] of Pointer;
+  TaskFill: TThread = nil;
+  TaskClear: TThread = nil;
 
 implementation
 
 {$R *.fmx}
 
+constructor TForm1.Create(AOwner: TComponent);
+begin
+  inherited;
+  FLck := TObject.Create;
+  FStop := False;
+  Randomize;
+  for var k := 0 to High(p) do
+  begin
+    p[k] := nil;
+    FSize[k] := 0;
+  end;
+end;
+
+destructor TForm1.Destroy;
+begin
+  FStop := True;
+
+  if Assigned(TaskFill) then
+  begin
+    TaskFill.WaitFor;
+    TaskFill.Free;
+    TaskFill := nil;
+  end;
+
+  if Assigned(TaskClear) then
+  begin
+    TaskClear.WaitFor;
+    TaskClear.Free;
+    TaskClear := nil;
+  end;
+
+  TMonitor.Enter(FLck);
+  try
+    for var k := 0 to High(p) do
+      if p[k] <> nil then
+      begin
+        FreeMem(p[k]);
+        p[k] := nil;
+        FSize[k] := 0;
+      end;
+  finally
+    TMonitor.Exit(FLck);
+  end;
+
+  FLck.Free;
+  inherited;
+end;
+
 procedure TForm1.bOpenFileClick(Sender: TObject);
 begin
-  for var k := 0 to 1000 do
-    p[k] := nil;
+  // Сначала диалог; при отмене — выходим и ничего не запускаем
+  if (OpenDialog1 = nil) or (not OpenDialog1.Execute) then
+    Exit;
 
-  If TaskFill = nil then begin
+  // Поток заполнения
+  if not Assigned(TaskFill) then
+  begin
     TaskFill := TThread.CreateAnonymousThread(
-    procedure
-    var
-      k:    integer;
-      N:    integer;
-    begin
-      repeat
-
-        for k := 0 to 1000 do If p[k] = nil then begin
-          N := random(10000);
-          GetMem(p[k], N);
-          FillChar(p[k]^, N, random(255));
+      procedure
+      begin
+        while not FStop do
+        begin
+          TMonitor.Enter(FLck);
+          try
+            for var k := 0 to High(p) do
+              if p[k] = nil then
+              begin
+                var N := 1 + Random(10000);
+                GetMem(p[k], N);
+                FSize[k] := N;
+                FillChar(p[k]^, FSize[k], Byte(Random(256)));
+              end;
+          finally
+            TMonitor.Exit(FLck);
+          end;
+          TThread.Sleep(10);
         end;
-
-        Sleep(10);
-
-      until false;
-
-    end);
-
+      end
+    );
+    TaskFill.FreeOnTerminate := False;
     TaskFill.Start;
   end;
 
-  If TaskClear = nil then begin
+  // Поток очистки
+  if not Assigned(TaskClear) then
+  begin
     TaskClear := TThread.CreateAnonymousThread(
-    procedure
-    var
-      k:    integer;
-      N:    integer;
-    begin
-      repeat
-
-        Sleep(10);
-
-        for k := 0 to 1000 do If p[k] <> nil then begin
-          FreeMem(p[k]);
-          p[k] := nil;
+      procedure
+      begin
+        while not FStop do
+        begin
+          TThread.Sleep(10);
+          TMonitor.Enter(FLck);
+          try
+            for var k := 0 to High(p) do
+              if p[k] <> nil then
+              begin
+                FreeMem(p[k]);
+                p[k] := nil;
+                FSize[k] := 0;
+              end;
+          finally
+            TMonitor.Exit(FLck);
+          end;
         end;
-
-      until false;
-
-    end);
-
+      end
+    );
+    TaskClear.FreeOnTerminate := False;
     TaskClear.Start;
   end;
-
-  OpenDialog1.Execute;
 end;
 
 procedure TForm1.bShowPopupClick(Sender: TObject);
 begin
-  Popup1.IsOpen := true;
+  Popup1.IsOpen := True;
 end;
 
 end.
-
